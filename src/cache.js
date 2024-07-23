7b427140b2f1be78f6c2f1fdf99a07157c88a21d
kartik2433 <95977507+kartik2433@users.noreply.github.com>
2024-07-23 23:21:53 +0530

    Environment Variable Loaded in Redis File

--- src/cache.js
+++ src/cache.js
@@ -1,17 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { createClient } from 'redis';
 
 const client = createClient({
     password: process.env.REDIS_PASSWORD,
     socket: {
-        host: process.env.REDIS_HOST,
+        host: `${process.env.REDIS_HOST}`,
         port: process.env.REDIS_PORT
     }
 });
+client.connect();
 
 client.on('error', (err) => {
   console.error('Redis Error:', err);
 });
 
-client.connect().catch(console.error);
+client.on('connect', () => {
+    console.log('Redis Connected');
+})
 
 export default client;
